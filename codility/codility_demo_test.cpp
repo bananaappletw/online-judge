0594dfd8fc9193a639d16c1d6ab0b7771fd70e85
bananaappletw <bananaappletw@gmail.com>
2017-03-30 15:50:05 +0800

    Add demo test

---
+++ codility/codility_demo_test.cpp
@@ -0,0 +1,14 @@
+int solution(vector<int> &A) {
+    int N = A.size();
+    long long int hash[N];
+    long long int sum =0;
+    for(int i=0;i<N;i++){
+        sum+=A[i];
+        hash[i]=sum;
+    }
+    for(int i=0;i<N;i++){
+        if(hash[i]-A[i] == sum - hash[i])
+            return i;
+    }
+    return -1;
+}
